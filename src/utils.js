73e944e2b07664d63a030db41049283c0ee5bd30
Aaroncho.dev <aaronchobusiness@gmail.com>
2023-05-18 09:11:02 -0300

    v3

--- src/utils.js
+++ src/utils.js
@@ -1,16 +1,3 @@
-import multer from "multer";
-
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, __dirname+'public')
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.originalname)
-    }
-  })
-  
-  export const upload = multer({ storage: storage })
-
 import path from "path";
 import { fileURLToPath } from "url";
 export const __filename = fileURLToPath(import.meta.url);
